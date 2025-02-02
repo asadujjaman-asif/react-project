65ee53f7f8396ad1e7024299362427f6a411488d
asadujjaman-asif <mdasadurjjaman@gmail.com>
2025-02-03 01:05:31 +0600

    task:added functionality of typing master v-1.0

--- src/components/textarea.tsx
+++ src/components/textarea.tsx
@@ -5,7 +5,7 @@ type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>
 
 const Textarea : FC<TextareaProps> = ({className, ...rest}) => {
   return (
-    <textarea className={twMerge("w-full px-2 py-2 rounded-lg text-sm text-slate-800 placeholder-slate-300 focus:border-slate-600 focus:outline-none focus:ring-1 focus:ring-slate-600 disabled:cursor-not-allowed disabled:opacity-50",className)} {...rest}>
+    <textarea className={twMerge("w-full px-4 py-2 border rounded-lg text-sm text-slate-800 placeholder-slate-300 focus:border-slate-600 focus:outline-none focus:ring-1 focus:ring-slate-600 disabled:cursor-not-allowed disabled:opacity-50",className)} {...rest}>
 
     </textarea>
   )
--- src/components/type-speed-dashboard.tsx
+++ src/components/type-speed-dashboard.tsx
@@ -1,57 +1,157 @@
-import React from 'react'
+import React, { useState,useRef } from 'react'
 import { FiRefreshCw } from "react-icons/fi";
 import { LuTimer } from "react-icons/lu";
 import Button from "./button";
 import Select from "./select";
 import Textarea from "./textarea";
 import SummaryCard from "./summary-card";
+import { twMerge } from 'tailwind-merge';
 
+const sampleTexts = [
+  "Technology is growing rapidly, changing how we live and work. Artificial intelligence, cloud computing, and cybersecurity play a major role in today’s digital world. Businesses rely on modern software and tools to stay competitive.",
+  "Web development continues to evolve with frameworks like React and Vue. As digital threats rise, cybersecurity experts work to keep data safe. Keeping up with new trends is essential for IT professionals to stay ahead in the industry.",
+  "The IT world is always changing, bringing new tools and innovations. Cloud computing allows businesses to store and process data efficiently. Artificial intelligence is helping companies automate tasks and make better decisions. Web development has advanced with JavaScript frameworks like React and Vue. Cybersecurity is a growing concern, with hackers constantly finding new ways to attack systems. The rise of remote work has increased the demand for collaboration tools. With new programming languages and trends emerging, IT professionals must continue learning. The future of technology depends on automation, data security, and user-friendly applications.",
+  "Technology has become an essential part of daily life. From artificial intelligence to cloud computing, businesses rely on modern solutions to operate efficiently. The rise of automation has improved productivity, but it also raises concerns about job security. Cybersecurity experts work hard to protect systems from hacking and data breaches.",
+  "Software development is evolving with languages like Python, JavaScript, and Go. Web applications are built using powerful frameworks like React and Vue. The backend is supported by tools like Node.js and Laravel, ensuring scalable performance.",
+  "The Internet of Things (IoT) is connecting devices, creating smarter homes and cities. Blockchain technology is transforming industries by providing secure and transparent transactions. As artificial intelligence grows, it enhances industries like healthcare, finance, and marketing.",
+  "Technology professionals must continuously learn and adapt to stay ahead. Online courses, coding boot camps, and certifications help keep skills updated. The future will bring even more innovations, shaping how we interact with the digital world.",
+];
 function TypeSpeed() {
+  const randomParagraph = (length:number=50) => {
+    const words = "Web development continues to evolve with frameworks like React and Vue. As digital threats rise, cybersecurity experts work to keep data safe. Keeping up with new trends is essential for IT professionals to stay ahead in the industry.".split(" ");
+    let paragraph = "";
+    for(let i = 0; i < words.length; i++){
+      paragraph += words[Math.floor(Math.random() * words.length)] + " ";
+    }
+    return paragraph;
+  }
+
+  const [value,setValue] = useState("");
+  const [duration,setDuration] = useState(30);
+  const [remaining,setRemaining] = useState(duration);
+  const [content,setContent] = useState(randomParagraph(100));
+  const [isTestRunning,setTestRunning] = useState(false);
+
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const [stats,setStats] = useState({wpm:0,cpm:0,accuracy:0,errors:0});
+  
+  const startTest = () => {
+
+    setTestRunning(true);
+    setRemaining(duration);
+
+    setStats({
+      wpm: 0,
+      cpm: 0,
+      accuracy: 0,
+      errors: 0,
+    });
+
+    intervalRef.current = setInterval(() => {
+      setRemaining((prev)=>{
+        if(prev===0){
+          resetTest();
+          return 0;
+        }
+        return prev-1;
+      });
+    },1000);
+  }
+
+  const resetTest = () => {
+    if(intervalRef.current){
+      clearInterval(intervalRef.current);
+    }
+    calculateStats();
+    setTestRunning(false);
+    setContent(randomParagraph(100));
+    setValue("");
+  }
+  const calculateStats = () => {
+    let words=value.trim().split(" ").length; 
+    let characters = value.length;
+
+    let timeSpent = (duration-remaining) / 60;
+
+    let wpm = Math.round(words / timeSpent);
+    let cpm = Math.round(characters / timeSpent);
+
+    let errors = 0;
+    let minLength = Math.min(content.length,value.length);
+    for (let i = 0; i < minLength; i++) {
+      if (content[i] !== value[i]) {
+        errors++;
+      }
+    }
+
+    let accuracy = Math.round(((characters - errors) / characters) * 100);
+    setStats({wpm,cpm,accuracy,errors});
+  }
+  const formatDuration=(duration:number) => {
+    return (
+      Math.floor(duration/60) + ":" + (duration % 60).toString().padStart(2,"0")
+    );
+  }
+  
+  const renderText = () => {
+    return content.split("").map((char,index) => {
+      let textColor = "";
+      if(value.length > index) {
+        const isCorrect = value[index] === char;
+        textColor = isCorrect ?"text-green-500":"text-red-800 font-bold";
+      }
+      return (
+        <span key ={index} className={textColor}>{char}</span>
+      );
+    });  
+  };
+
+ 
+  
+
   return (
     <>
       <main className='h-screen flex items-center justify-center  p-6'>
-          <section className='border p-6 rounded-lg bg-white w-full max-w-4xl'>
-            <div className='flex'>
+          <section className='border p-6 rounded-lg bg-white w-full max-w-4xl mt-4'>
+            <div className='flex flex-col sm:flex-row'>
                 <p className='text-xl font-semibold'>Typing Speed Calculator</p>
                 <div className='ms-auto flex items-center gap-5'>
-                <Select>
+                <Select value={duration.toString()} onValueChange={(value)=>setDuration(parseInt(value))}>
                     <Select.Trigger className="w-32">
                       <Select.Value placeholder="Select" />
                     </Select.Trigger>
                     <Select.Content className="p-2">
-                      <Select.Item value="1">1 Minute</Select.Item>
-                      <Select.Item value="2">2 Minutes</Select.Item>
-                      <Select.Item value="5">5 Minutes</Select.Item>
+                      <Select.Item value="30">30 seconds</Select.Item>
+                      <Select.Item value="60">1 Minute</Select.Item>
+                      <Select.Item value="120">2 Minutes</Select.Item>
                     </Select.Content>
                   </Select>
-                  <Button className="inline-flex items-center gap-2">
+                  <Button className={twMerge("inline-flex items-center gap-2",isTestRunning?"bg-red-600 hover:bg-red-700":"bg-slate-800 hover:bg-slate-900")} onClick={isTestRunning?resetTest:startTest}>
                     <FiRefreshCw size={18} />
-                    Start
+                    {isTestRunning?'Reset':'Start'}
                   </Button>
                 </div>
             </div>
             <div className='flex items-center justify-center gap-2 mt-4'>
                   <LuTimer size={18}/>
-                  <p className='text-xl font-semibold'>1:00</p>
+                  <p className='text-xl font-semibold'>{isTestRunning?formatDuration(remaining):formatDuration(duration)}</p>
               </div>
               <div className='bg-slate-50 rounded-lg p-6 mt-4'>
-                  <p className='text-slate-500 text-justify'>Technology continues to evolve at a rapid pace. What was
-                  cutting-edge yesterday might be obsolete tomorrow. Adaptability and
-                  continuous learning are key skills in this field. The ability to be
-                  flexible and learn new skills is essential for a successful career
-                  in technology.</p>
+                  <p className='text-slate-500 text-justify'>{renderText()}</p>
               </div>
               <Textarea
                 className="mt-4"
                 rows={4}
                 placeholder="Type to begin the test"
-                disabled
+                disabled={!isTestRunning}
+                value={value}
+                onChange={(env:any) =>setValue(env.target.value)}
               />
-             <div className='mt-6 grid grid-cols-4 gap-4'>
-              <SummaryCard value="0" unit ="WPM" />
-              <SummaryCard value="0" unit ="CPM" />
-              <SummaryCard value="0%" unit ="Accuracy" />
-              <SummaryCard value="0" unit ="Errors" />
+             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4'>
+              <SummaryCard value={stats.wpm.toString()} unit ="WPM" />
+              <SummaryCard value={stats.cpm.toString()} unit ="CPM" />
+              <SummaryCard value={`${stats.accuracy}%`} unit ="Accuracy" />
+              <SummaryCard value={stats.errors.toString()} unit ="Errors" />
              </div>
           </section>
       </main>
