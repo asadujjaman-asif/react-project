f5912d2a8a0b58670ccb9be7d82d45ec94f81a41
asadujjaman-asif <mdasadurjjaman@gmail.com>
2025-03-12 00:24:26 +0600

    added success message

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import InputFiled from './components/data-mutation'
+const queryClient = new QueryClient(); 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <InputFiled/>
+      
+      <QueryClientProvider client={queryClient}>
+       {/**  <Suspense fallback={<div>Loading...</div>} > */}
+       <InputFiled/>
+          {/** </Suspense>*/}
+      </QueryClientProvider>
     </>
   )
 }
--- src/components/data-mutation.tsx
+++ src/components/data-mutation.tsx
@@ -1,4 +1,5 @@
 import {React,FC,useState} from 'react'
+import { useMutation,QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 type ProductFormData = {
   title:string;
@@ -14,6 +15,11 @@ type FormError = {
   categoryId?:string;
   images?:string;
 }
+const categories = [
+  {id:1,name:'Electronics'},
+  {id:2,name:'Shoes'},
+  {id:3,name:'Components'},
+];
 const DataMutation:React.FC=(any)=> {
 
   const initialFormData:ProductFormData = {
@@ -26,12 +32,32 @@ const DataMutation:React.FC=(any)=> {
   
   const [formData,setFormData] = useState<ProductFormData>(initialFormData);
   const [errors,setErrors] = useState<FormError>({});
+  
+  const mutation = useMutation({
+      mutationKey:['create-product'],
+      mutationFn:async ()=>
+        fetch("https://api.escuelajs.co/api/v1/products/",{
+          method:"POST",
+          headers:{
+            "Content-type":"application/json",
+          },
+          body:JSON.stringify(formData)
+        }),
+      onSuccess: ()=>{
+        setFormData(initialFormData);
+      },
+      onError:()=>{
 
+      }
+
+  });
+  
   const handleSubmit = async(e: React.FormEvent<HTMLFormElement>)=>{
       e.preventDefault();
       if(!validateData()){
           return;
       }
+      mutation.mutate();
   }
  
   const validateData = ()=>{
@@ -49,7 +75,7 @@ const DataMutation:React.FC=(any)=> {
     if(!formData.categoryId){
       errors.categoryId = "Category is required";
     }
-    if(formData.images){
+    if(formData.images.length==0){
       errors.images = "Images is required";
     }
     setErrors(errors);
@@ -60,6 +86,7 @@ const DataMutation:React.FC=(any)=> {
       <div className='flex w-full items-center justify-center h-screen '>
         <div className='border border-stroke w-1/2 rounded-md bg-white p-6 shadow-lg font-mono'>
           <p className='font-bold py-2 text-lg text-left'>Create new products</p>
+          {mutation.isSuccess && (<p className='bg-green-100 border border-green-500 py-3 font-bold text-green-700'>Product created successfully.</p>)}
           <form onSubmit={handleSubmit}>
             <div className='mr-2 ml-2 py-2'>
               <p className='text-left text-sm'>Product name</p>
@@ -68,33 +95,35 @@ const DataMutation:React.FC=(any)=> {
             </div>
             <div className='mr-2 ml-2 py-2'>
               <p className='text-left text-sm'>Product Price</p>
-              <input className={`border ${errors.price?"border-red-500 focus:ring-red-300 focus:ring-1":" focus:outline-none focus:ring-0"} rounded-md w-full  bg-transparent py-2 text-base text-body-color px-3`} type="number" placeholder='Product price...' value={formData.price?.toString()} onChange={(e)=>setFormData({...formData, price:parseInt( e.target.value)})}/>
+              <input className={`border ${errors.price?"border-red-500 focus:ring-red-300 focus:ring-1":" focus:outline-none focus:ring-0"} rounded-md w-full  bg-transparent py-2 text-base text-body-color px-3`} type="number" placeholder='Product price...' value={formData.price?.toString() || ""} onChange={(e)=>setFormData({...formData, price:parseInt( e.target.value)})}/>
               {errors.price && (<p className='text-xs text-red-500 text-left'>{errors.price}.</p>)}
             </div>
             <div className='mr-2 ml-2 py-2'>
               <p className='text-left text-sm'>Description</p>
-              <textarea className={`border ${errors.description?"border-red-500 focus:ring-red-300 focus:ring-1":" focus:outline-none focus:ring-0"} rounded-md w-full bg-transparent py-2 px-3 text-base text-body-color h-24 resize-none`} placeholder="Write description here..." value={formData.title} onChange={(e)=>setFormData({...formData,description:e.target.value})}>
+              <textarea className={`border ${errors.description?"border-red-500 focus:ring-red-300 focus:ring-1":" focus:outline-none focus:ring-0"} rounded-md w-full bg-transparent py-2 px-3 text-base text-body-color h-24 resize-none`} placeholder="Write description here..." value={formData.description} onChange={(e)=>setFormData({...formData,description:e.target.value})}>
 
               </textarea>
               {errors.description && (<p className='text-xs text-red-500 text-left'>{errors.description}.</p>)}
             </div>
             <div className='mr-2 ml-2 py-2'>
               <p className='text-left text-sm'>Category</p>
-              <select className={`border ${errors.categoryId?"border-red-500 focus:ring-red-300 focus:ring-1":" focus:outline-none focus:ring-0"} rounded-md w-full bg-white py-2 px-3 text-base text-gray-700`} value={formData.categoryId?.toString()} onChange={(e)=>setFormData({...formData,categoryId:parseInt(e.target.value)})}>
+              <select className={`border ${errors.categoryId?"border-red-500 focus:ring-red-300 focus:ring-1":" focus:outline-none focus:ring-0"} rounded-md w-full bg-white py-2 px-3 text-base text-gray-700`} value={formData.categoryId?.toString() || ""} onChange={(e)=>setFormData({...formData,categoryId:parseInt(e.target.value)})}>
                 <option value="">Select an option</option>
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
               </select>
               {errors.categoryId && (<p className='text-xs text-red-500 text-left'>{errors.categoryId}.</p>)}
             </div>
             <div className='mr-2 ml-2 py-2'>
               <p className='text-left text-sm'>Image Url</p>
-              <input className={`border ${errors.images?"border-red-500 focus:ring-red-300 focus:ring-1":" focus:outline-none focus:ring-0"} rounded-md w-full  bg-transparent py-2 text-base text-body-color px-3`} type="text" placeholder='Image Url...' value={formData.images[0]} onChange={(e)=>setFormData({...formData,images:[e.target.value]})}/>
+              <input className={`border ${errors.images?"border-red-500 focus:ring-red-300 focus:ring-1":" focus:outline-none focus:ring-0"} rounded-md w-full  bg-transparent py-2 text-base text-body-color px-3`} type="text" placeholder='Image Url...' value={formData.images[0] || ""} onChange={(e)=>setFormData({...formData,images:[e.target.value]})}/>
               {errors.images && (<p className='text-xs text-red-500 text-left'>{errors.images}.</p>)}
             </div>
             <div className='mr-2 ml-2 py-2'>
-              <button className='bg-primary-500 w-full text-white py-2 rounded-md hover:bg-primary-700' type="submit">Create Product</button>
+              <button className='bg-primary-500 w-full text-white py-2 rounded-md hover:bg-primary-700 disabled:opacity-50 disabled:cursor-not-allowed' disabled={mutation.isPending} type="submit">
+                {mutation.isPending?"Creating...":"Create Product"}
+              </button>
              </div>
           </form>
         </div>
